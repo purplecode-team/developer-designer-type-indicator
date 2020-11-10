030c9279b23a33bbbb3cbbdc77e6c61a0a46e710
JangJua <jua0610@naver.com>
2020-11-10 14:43:49 +0900

    modify: media 상수 변경

--- src/lib/styles/media.js
+++ src/lib/styles/media.js
@@ -1,4 +1,4 @@
-const size = {
+const media = {
   mobileS: '320px',
   mobileM: '375px',
   mobileL: '425px',
@@ -6,17 +6,6 @@ const size = {
   laptop: '1024px',
   laptopL: '1440px',
   desktop: '2560px',
-};
-
-const media = {
-  mobileS: `(min-width: ${size.mobileS})`,
-  mobileM: `(min-width: ${size.mobileM})`,
-  mobileL: `(min-width: ${size.mobileL})`,
-  tablet: `(min-width: ${size.tablet})`,
-  laptop: `(min-width: ${size.laptop})`,
-  laptopL: `(min-width: ${size.laptopL})`,
-  desktop: `(min-width: ${size.desktop})`,
-  desktopL: `(min-width: ${size.desktop})`,
 };
 
 export default media;
