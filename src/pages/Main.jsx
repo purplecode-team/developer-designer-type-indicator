f99966d0c4a962ff461f9c1e55c8f7688823ff52
JangJua <jua0610@naver.com>
2020-11-10 12:06:46 +0900

    add: GlobalStyle 적용

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
+import styled from 'styled-components';
+import bg from '../../public/img/bg.png';
+import { media } from '../lib/styles/media';
 
 const Main = () => {
   return (
-    <>
+    <MainContainer>
       <h1> Home </h1>
-    </>
+    </MainContainer>
   );
 };
+
+const MainContainer = styled.div`
+  height: 100vh;
+  background-image: url(${bg});
+  background-repeat: no-repeat;
+`;
 
 export default Main;
