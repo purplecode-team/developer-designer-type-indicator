6c1dfb326f16e0b82bc9fee35be73343c05123d2
JangJua <jua0610@naver.com>
2020-11-15 23:10:04 +0900

    add: 풀, 구름 배경을 common으로 분리

--- src/containers/main/MainContainer.jsx
+++ src/containers/main/MainContainer.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import styled from 'styled-components';
-import img from '../../../public/img/bg_clouds.png';
+import cloudImg from '../../../public/img/bg_clouds.png';
 import grassImg from '../../../public/img/bg_bottom.png';
-import media from '../../lib/styles/media';
+import CloudBackground from '../../components/common/CloudBackground';
+import GrassBackground from '../../components/common/GrassBackground';
 import TurtleWithTitle from '../../components/main/TurtleWithTitle';
 
 const Container = styled.div`
@@ -10,42 +11,16 @@ const Container = styled.div`
   max-width: 100%;
 `;
 
-const CloudBackground = styled.span`
-  margin: 0 auto;
-  max-width: ${media.laptopL};
-  height: 100%;
-  display: block;
-  background-image: url(${img});
-  background-repeat: no-repeat;
-  background-position: 50% 30%;
-  background-size: 60%;
-  @media (max-width: ${media.laptopL}) {
-    background-size: 80%;
-    background-position: 50% 10%;
-  }
-  @media (max-width: ${media.tablet}) {
-    background-size: 95%;
-  }
-`;
-
-const GrassBackground = styled.span`
-  width: 100%;
-  height: 100%;
-  display: block;
-  position: relative;
-  top: -200%;
-  background-image: url(${grassImg});
-  background-position: bottom;
-  background-size: 100% 15rem;
-  background-repeat: no-repeat;
-`;
-
 const MainContainer = () => {
   return (
     <Container>
-      <CloudBackground role="img" aria-label="clouds background" />
+      <CloudBackground
+        role="img"
+        ariaLabel="clouds background"
+        img={cloudImg}
+      />
       <TurtleWithTitle />
-      <GrassBackground role="img" aria-label="grass background" />
+      <GrassBackground role="img" ariaLabel="grass background" img={grassImg} />
     </Container>
   );
 };
