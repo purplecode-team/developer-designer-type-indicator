f131dc067246c34a754ce88db822b2852251160c
highcastle4242 <wnstjd4242@naver.com>
2020-11-18 11:12:46 +0900

    modify: button function

--- src/components/test/Content.jsx
+++ src/components/test/Content.jsx
@@ -36,29 +36,35 @@ const Content = ({nextSlide, data}) => {
   const [result, setResult] = useState({
     E:0, I:0 , T:0, F:0, J:0, P:0
   });
-  const getAnswer = (id) =>{
-    
+
+  useEffect(()=>{
     console.log(count);
     console.log(result);
-    // if(id === "EI"){
-    //   setCount(result.E + 1);
-    //   setResult({E:count});
-    //   console.log('getAnswer 실행');
-    // }else if(id === "TF"){
-    //   setCount(result.T + 1);
-    //   setResult({T:count});
-    //   console.log('getAnswer 실행');
-    // }else if(id === "JP"){
-    //   setCount(result.J + 1);
-    //   setResult({J:count});
-    //   console.log('getAnswer 실행');
-    // }else{
-    //   console.log('data.id is wrong');
-    // }
+  },[result]);
+  const getAnswer = (id) =>{
+    console.log(id);
+    // console.log(count);
+    // console.log(result);
+    // console.log('getAnswer 실행');
+    if(id === "EI"){
+      setCount(result.E + 1);
+      setResult({...result,E:result.E + 1});
+      console.log('getAnswer EI 실행');
+    }else if(id === "TF"){
+      setCount(result.T + 1);
+      setResult({...result,T:result.T + 1});
+      console.log('getAnswer TF 실행');
+    }else if(id === "JP"){
+      setCount(result.J + 1);
+      setResult({...result,J:result.J + 1});
+      console.log('getAnswer JP 실행');
+    }else{
+      console.log('data.id is wrong');
+    }
   };
   
   const clickSelection = (id)=>{
-    nextSlide;
+    nextSlide();
     getAnswer(id);
     console.log('click실행 ');
   };
@@ -71,10 +77,10 @@ const Content = ({nextSlide, data}) => {
         </SlideText>
       </SlideTextWrap>
       <SlideBtnBox>
-        <SlideBtnButton type="button" onClick={clickSelection(data.id)}>
+        <SlideBtnButton type="button" onClick={()=>{clickSelection(data.id)}}>
           {data.A}
         </SlideBtnButton>
-        <SlideBtnButton type="button" onClick={clickSelection(data.id)}>
+        <SlideBtnButton type="button" onClick={()=>{clickSelection(data.id)}}>
           {data.B}
         </SlideBtnButton>
       </SlideBtnBox>
