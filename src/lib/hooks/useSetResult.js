87bd92360a629465fab1edf81b0484b032abc096
highcastle4242 <wnstjd4242@naver.com>
2020-12-31 19:41:13 +0900

    modify: Test logic

--- src/lib/hooks/useSetResult.js
+++ src/lib/hooks/useSetResult.js
@@ -3,23 +3,31 @@ import { useReducer } from 'react';
 
 const reducer = (state, action) => {
   switch (action.type) {
-    case 'EI':
+    case 'E':
       state.E += 1;
       return { ...state };
-    case 'TF':
+    case 'I':
+      state.I += 1;
+      return { ...state };
+    case 'T':
       state.T += 1;
       return { ...state };
-    case 'JP':
+    case 'F':
+      state.F += 1;
+      return { ...state };
+    case 'J':
       state.J += 1;
       return { ...state };
+    case 'P':
+      state.P += 1;
+      return { ...state };
     default:
-
       return state;
   }
 };
 
 export default function useSetResult() {
-    const [state, dispatch] = useReducer(reducer, { E: 0, T: 0, J: 0 });
+    const [state, dispatch] = useReducer(reducer, { E: 0, I: 0, T: 0, F: 0, J: 0, P: 0});
 
     return [state, dispatch];
 };
